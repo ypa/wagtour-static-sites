c6c9350fe772ee1954676ec47a2508c3d1d5b090
Yan Aung <yan.pye.aung@gmail.com>
2014-04-28 20:48:57 -0700

    Add js for cycling through thumbnails

--- www.wagtour.com/static/js/main.js
+++ www.wagtour.com/static/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 	console.log("Hello!");
+
+	$("#thumbs img").first().addClass("thumb-active");
+	$("#thumbs img").first().removeClass("grayscale");
+
+	$("#thumbs img").click(function() {
+		$("#thumbs img").addClass("grayscale");
+		$("#thumbs img").removeClass("thumb-active");
+		$(this).removeClass("grayscale");
+		$(this).addClass("thumb-active");
+		$('#main-img img').attr('src',$(this).attr('src'));
+	});
 });
