656c9862e744cbabc545224e291cabe7b26cdd43
Yan Aung <yan.pye.aung@gmail.com>
2014-05-16 20:59:03 -0700

    Add Google Analytics

--- www.wagtour.com/static/js/main.js
+++ www.wagtour.com/static/js/main.js
@@ -101,3 +101,17 @@ $("#submit").click(function(event){
     // prevent default posting of form
     event.preventDefault();
 });
+
+
+/*========================
+Google Analytics Code
+=========================*/
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-38777542-2', 'sittown.com');
+ga('send', 'pageview');
+
